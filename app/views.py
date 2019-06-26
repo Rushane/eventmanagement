67463bee118a23f84fd42b7cf95daf5c595ea975
akeam <akeam.williams@yahoo.com>
2019-06-26 15:30:04 -0600

    "add token and update login."

--- app/views.py
+++ app/views.py
@@ -12,10 +12,31 @@ from app.forms import LoginForm, RegistrationForm, EventForm
 from app.models import EventManager, Event
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import request, jsonify, make_response
-
+import uuid
+import jwt
+import datetime
+from functools import wraps
 ###
 # Routing for your application.
 ###
+
+# authenticated user by give them a token
+def token_required(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        token=None
+        if 'x-access-token' in request.headers:
+            token=request.headers['x-access-token']
+        if not token:
+            return jsonify({'message':'Token is not present!'})
+        try:
+            data =jwt.decode(token,app.config['SECRET_KEY'])
+            current_user=EventManager.query.filter_by(public_id=data['public_id']).first()
+        except Exception as e:
+            return jsonify({'message':'Not a valid token!'}),401
+        return f(current_user,*args,**kwargs)
+    return decorated
+
 
 @app.route('/')
 def home():
@@ -23,7 +44,7 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/about/')
+@app.route('/about')
 def about():
     """Render the website's about page."""
     return render_template('about.html')
@@ -31,41 +52,33 @@ def about():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
-    if request.method == "POST":
-        # change this to actually validate the entire form submission
-        # and not just one field
-        if form.username.data:
-            # Get the username and password values from the form.
+    # auth = request.authorization
+    # if not auth or not auth.username or not auth.password:
+    #     return make_response("Authentication not verified",401,{"WWW-Authenticate":'Basic realm="Login Requried!"'})
+    # user = User.query.filter_by(name=auth.username).first()
+    # if not user:
+    #     return make_response("Authentication not verified",401,{"WWW-Authenticate":'Basic realm="Login Requried!"'})
+    # if check_password_hash(user.password,auth.password):
+    #     token = jwt.encode({'public_id':user.public_id,'exp':datetime.datetime.utcnow()+datetime.timedelta(minutes=30)},app.config["SECRET_KEY"])
+    #     return jsonify({'token':token.decode('UTF-8')})
+    # return make_response("Authentication not verified",401,{"WWW-Authenticate":'Basic realm="Login Requried!"'})
+    pass
 
-            # using your model, query database for a user based on the username
-            # and password submitted. Remember you need to compare the password hash.
-            # You will need to import the appropriate function to do so.
-            # Then store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method below.
-
-            # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
-    return render_template("login.html", form=form)
-
-@app.route("/api/users/register",  methods=["POST"]) 
+@app.route("/api/users/register",  methods=["POST"])
 def register():
     data = request.get_json()
     pword_hashed = generate_password_hash(data['password'],method='sha256')
-    new_user = EventManager(first_name=data['first_name'], last_name=data['last_name'], email=data['email'], 
+    new_user = EventManager(first_name=data['first_name'], last_name=data['last_name'], email=data['email'],
     telnum=data['telnum'] ,username=data['username'], password=pword_hashed, admin=False)
-        
+
     db.session.add(new_user)
     db.session.commit()
 
     return jsonify({'message': 'The Event Manager user was created'})
 
 # how to post image in postman: https://stackoverflow.com/questions/39660074/post-image-data-using-postman
-@app.route("/api/events/createEvent",  methods=["POST"]) 
-def createNewEvent():        
+@app.route("/api/events/createEvent",  methods=["POST"])
+def createNewEvent():
     #date_created = datetime.datetime.now().strftime("%B %d, %Y")
     data = request.get_json()
     new_event = Event(name=data['name'], title=data['title'], category=data['category'], start_date=data['start_date'] ,
@@ -73,38 +86,38 @@ def createNewEvent():
     # The picture thingy with Flyer not working
     db.session.add(new_event)
     db.session.commit()
-    
+
     return jsonify({'message': 'Event was created'})
 
 # Should but not tested
 @app.route('/api/events/<eventID>/public', methods=['PUT'])
 def makeEventPublic(eventid):
     record = Event.query.filter_by(eventid=eventid).first()
-    
+
     record.eventstatus = "Public"
     db.session.commit()
-    
+
     return jsonify({'message':'Event was made public'})
 
 @app.route('/api/events/<eventid>/viewEventInfo', methods=['GET'])
 def viewEventInfo(eventid):
     """Render details of particular event."""
     result=Event.query.filter_by(eventid=eventid).first()
-    
-    return render_template('viewEvent.html', result=result) 
-    
+
+    return render_template('viewEvent.html', result=result)
+
 @app.route("/api/events/<eventid>/update", methods=['GET', 'POST'])
-def updateEventInfo(eventid): 
+def updateEventInfo(eventid):
     record = Event.query.filter_by(eventid=eventid).first()
     form = EventForm(request.form, obj=record)
-    
+
     if request.method == "POST" and form.validate_on_submit():
         form.populate_obj(record)
-            
+
         db.session.commit()
         flash('Event {} Updated!'.format(record.eventid), 'success')
         return redirect(url_for("home"))
-    
+
     flash_errors(form)
     return render_template('updateEvent.html', form=form, record=record)
 
@@ -116,7 +129,7 @@ def deleteEvent(eventid):
         return jsonify({'message':'This event does not exist!'})
     db.session.delete(eventrecord)
     db.session.commit()
-    
+
     return jsonify({'message':'Event was deleted'})
 
 # user_loader callback. This callback is used to reload the user object from
