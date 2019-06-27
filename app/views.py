0318e4e6da7161171b099fc69f5e24f9afc6cce6
akeam <akeam.williams@yahoo.com>
2019-06-27 10:37:40 -0600

    update view.py

--- app/views.py
+++ app/views.py
@@ -5,11 +5,10 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 import os
-from app import app, db, login_manager
+from runapp import app, db
 from flask import render_template, request, redirect, url_for, flash
-from flask_login import login_user, logout_user, current_user, login_required
-from app.forms import LoginForm, RegistrationForm, EventForm
-from app.models import EventManager, Event
+from forms import LoginForm, RegistrationForm, EventForm
+from models import *
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import request, jsonify, make_response
@@ -38,7 +37,6 @@ def token_required(f):
         return f(current_user,*args,**kwargs)
     return decorated
 
-
 @app.route('/')
 def home():
     """Render website's home page."""
@@ -50,167 +48,173 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
     auth = request.authorization
     if not auth or not auth.username or not auth.password:
         return make_response("Authentication not verified",401,{"WWW-Authenticate":'Basic realm="Login Requried!"'})
-    user = EventManager.query.filter_by(name=auth.username).first()
+    user = EventManager.query.filter_by(username=auth.username).first()
     if not user:
         return make_response("Authentication not verified",401,{"WWW-Authenticate":'Basic realm="Login Requried!"'})
     if check_password_hash(user.password,auth.password):
-        token = jwt.encode({'public_id':user.public_id,'exp':datetime.datetime.utcnow()+datetime.timedelta(minutes=30)},app.config["SECRET_KEY"])
+        token = jwt.encode({'public_id':user.manager_publicId,'exp':datetime.datetime.utcnow()+datetime.timedelta(minutes=30)},app.config["SECRET_KEY"])
         return jsonify({'token':token.decode('UTF-8')})
     return make_response("Authentication not verified",401,{"WWW-Authenticate":'Basic realm="Login Requried!"'})
-    pass
 
-# def admin_login_required():
-#     def wrap(*args, **kwargs):
-#         # user is available from @login_required
-#         if not g.user.is_admin:
-#             return "you need to be admin", 401
-# return f(*args, **kwargs)
-
-@app.route("/api/users/register",  methods=["POST"])
+@app.route("/api/users/register", methods=["GET","POST"])
 def register():
-    data = request.get_json()
-    pword_hashed = generate_password_hash(data['password'],method='sha256')
-    new_user = EventManager(first_name=data['first_name'], last_name=data['last_name'], email=data['email'],
-    telnum=data['telnum'] ,username=data['username'], password=pword_hashed, admin=False)
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({'message': 'The Event Manager user was created'})
+    form=RegistrationForm()
+    if request.method == 'POST' :#and form.validate_on_submit():
+        data = request.get_json()
+        pword_hashed = data['password'] #generate_password_hash(data['password'],method='sha256')
+        new_user = EventManager(first_name=data['first_name'], last_name=data['last_name'], email=data['email'],telnum=data['telnum'] ,username=data['username'], password=pword_hashed, admin=False)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'A new Event Manager user was created'})
+    return render_template('register.html',form=form)
 
 # how to post image in postman: https://stackoverflow.com/questions/39660074/post-image-data-using-postman
-#@token_required
 @app.route("/api/events/createEvent",  methods=["POST"])
+#@token_required
 def createNewEvent():
-    # Getting 404 not found for some reason
-    #date_created = datetime.datetime.now().strftime("%B %d, %Y")
-    data = request.get_json()
-    #print(data)
-    #imagefile = request.files.get('imagefile', '')
-    #print(imagefile)
-    print(data)
-    new_event = Event(name=data['name'], title=data['title'], category=data['category'], start_date=data['start_date'] ,
-    end_date=data['end_date'] ,description=data['description'], cost=data['cost'],venue=data['venue'], flyer=data['flyer'],
-    managerid=data['managerid'], public=False)
-    # The picture thingy with Flyer not working
+    data =request.form
+    files = request.files['flyer']
+    photo=files
+    filename= secure_filename(photo.filename)
+    photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    new_event = Event(name=data['name'], title=data['title'], category=data['category'], start_date=datetime.datetime.strptime(data['start_date'],"%Y/%m/%d"),
+    end_date=datetime.datetime.strptime(data['end_date'],"%Y/%m/%d") ,description=data['description'], cost=data['cost'],venue=data['venue'], flyer=filename, managerid=data['managerid'])
+
     db.session.add(new_event)
     db.session.commit()
 
     return jsonify({'message': 'Event was created'})
 
-@app.route('/api/events/<eventID>/public', methods=['PUT'])
+# Should but not tested
+@app.route('/api/events/<event_publicId>/public', methods=['PUT'])
 #@token_required
-def makeEventPublic(eventid):
-    record = Event.query.filter_by(eventid=eventid).first()
-    
-    record.public = True
-    db.session.commit()
+def makeEventPublic(event_publicId):
+    record = Event.query.filter_by(event_publicId=event_publicId).first()
+    if record is None:
+        return jsonify({'message':'No events exist!'})
+    else:
+        record.public = True
+        db.session.commit()
+        return jsonify({'message':'Event was made public'})
 
-    return jsonify({'message':'Event was made public'})
-
-@app.route('/api/events/<eventid>/viewEventInfo', methods=['GET'])
-def viewEventInfo(eventid):
-    event = Event.query.filter_by(eventid=eventid).first()
+@app.route('/api/events/<event_publicId>/viewEventInfo', methods=['GET'])
+def viewEventInfo(event_publicId):
+    event = Event.query.filter_by(event_publicId=event_publicId).first()
     if not event:
         return jsonify({'message':'This event does not exist!'})
-    event_dict = dict()
-    event_dict['eventid'] = event.eventid
-    event_dict['name'] = event.name
-    event_dict['title'] = event.title 
-    event_dict['category'] = event.category
-    event_dict['start_date'] = event.start_date
-    event_dict['end_date'] = event.end_date
-    event_dict['description'] = event.description
-    event_dict['venue'] = event.venue
-    event_dict['flyer'] = event.flyer
-
-    return jsonify({'message':'Event shown!'})
-
-@app.route("/api/events/<eventid>", methods=['PUT'])
-def updateEventInfo(eventid): 
-    record = Event.query.filter_by(eventid=eventid).first()
-    #form = EventForm(request.form, obj=record)
-    name =  request.json['name'] 
-    title = request.json['title']
-    category = request.json['category']
-    start_date = request.json['start_date']
-    end_date = request.json['end_date']
-    description = request.json['description']
-    venue = request.json['venue']
-    flyer = request.json['flyer']
-
-    record.name = name
-    record.title = title
-    record.category = category
-    record.start_date = start_date
-    record.end_date = end_date
-    record.description = description
-    record.venue = venue
-    record.flyer = flyer
-
-    db.session.commit()
-    return jsonify({'message':'Event data was changed'})
-    
-@app.route('/api/events/<eventid>/delete', methods=['DELETE'])
-#@token_required
-def deleteEvent(eventid):
-    """ deletes event """
-    eventrecord =  Event.query.filter_by(eventid=eventid).first()
-    if not eventrecord:
-        return jsonify({'message':'This event does not exist!'})
-    db.session.delete(eventrecord)
-    db.session.commit()
-
-    return jsonify({'message':'Event was deleted'})
-
-# endpoint to get event detail by name
-@app.route("/api/events/search/<eventname>", methods=["GET"])
-def searchForEvent(eventname):
-    events = Event.query.filter_by(name=eventname)
-    event_list = []
-    for event in events:
+    else:
         event_dict = dict()
         event_dict['eventid'] = event.eventid
-        event_dict['event_publicId'] = event.event_publicId
         event_dict['name'] = event.name
-        event_dict['title'] = event.title 
+        event_dict['title'] = event.title
         event_dict['category'] = event.category
         event_dict['start_date'] = event.start_date
         event_dict['end_date'] = event.end_date
         event_dict['description'] = event.description
         event_dict['venue'] = event.venue
         event_dict['flyer'] = event.flyer
-        #event_dict['managerid'] = event.managerid
-    return jsonify({'events': event_list}), 200
+        event_dict['managerid'] = event.managerid
+        return jsonify({'message':event_dict})
 
-@app.route("/api/events/getAllEvents", methods="GET")
+@app.route("/api/events/<event_publicId>", methods=['PUT'])
+def updateEventInfo(event_publicId):
+    record = Event.query.filter_by(event_publicId=event_publicId).first()
+    if record is None:
+        return jsonify({'message':'No events exist!'})
+    else:
+        data =request.form
+        files = request.files['flyer']
+        photo=files
+        filename= secure_filename(photo.filename)
+        # get data FROM THE FORM
+        name =  data['name']
+        title = data['title']
+        category = data['category']
+        start_date = datetime.datetime.strptime(data['start_date'],"%Y/%m/%d")
+        end_date = datetime.datetime.strptime(data['end_date'],"%Y/%m/%d")
+        description = data['description']
+        cost=data['cost']
+        venue = data['venue']
+        flyer = filename
+        managerid = data['managerid']
+        # ADD data to database
+        record.name = name
+        record.title = title
+        record.category = category
+        record.start_date = start_date
+        record.end_date = end_date
+        record.description = description
+        record.cost=cost
+        record.venue = venue
+        record.flyer = flyer
+        record.managerid = managerid
+        db.session.commit()
+        return jsonify({'message':'Event data was changed'})
+
+@app.route('/api/events/<event_publicId>', methods=['DELETE'])
+#@token_required
+def deleteEvent(event_publicId):
+    """ deletes event """
+    eventrecord =  Event.query.filter_by(event_publicId=event_publicId).first()
+    if not eventrecord:
+        return jsonify({'message':'This event does not exist!'})
+    else:
+        db.session.delete(eventrecord)
+        db.session.commit()
+        return jsonify({'message':'Event was deleted'})
+
+# endpoint to get user detail by id
+@app.route("/api/events/search/<eventname>", methods=["GET"])
+def searchForEvent(eventname):
+    events = Event.query.filter_by(name=eventname).all()
+    event_list = []
+
+    if events == []:
+        return jsonify({'message':'This event does not exist!'})
+    else:
+        for event in events:
+            event_dict = dict()
+            event_dict['eventid'] = event.eventid
+            event_dict['event_publicId'] = event.event_publicId
+            event_dict['name'] = event.name
+            event_dict['title'] = event.title
+            event_dict['category'] = event.category
+            event_dict['start_date'] = event.start_date
+            event_dict['end_date'] = event.end_date
+            event_dict['description'] = event.description
+            event_dict['venue'] = event.venue
+            event_dict['flyer'] = event.flyer
+            event_dict['managerid'] = event.managerid
+            event_list.append(event_dict)
+        return jsonify({'events': event_list})
+@app.route("/api/events/getAllEvents", methods=["GET"])
 def getAllEvents():
     events = Event.query.all()
     event_list = []
+    if events is None:
+        return jsonify({'message':'No events exist!'})
     for event in events:
         event_dict = dict()
         event_dict['eventid'] = event.eventid
         event_dict['event_publicId'] = event.event_publicId
         event_dict['name'] = event.name
-        event_dict['title'] = event.title 
+        event_dict['title'] = event.title
         event_dict['category'] = event.category
         event_dict['start_date'] = event.start_date
         event_dict['end_date'] = event.end_date
         event_dict['description'] = event.description
         event_dict['venue'] = event.venue
         event_dict['flyer'] = event.flyer
-        #event_dict['managerid'] = event.managerid
+        event_dict['managerid'] = event.managerid
+        event_list.append(event_dict)
     return jsonify({'events': event_list})
 
-    
-
-@app.route("/api/events/<eventid>/comment",methods="POST")
+@app.route("/api/events/<eventid>/comment",methods=["POST"])
 def commentOnEvent(eventid):
     """ comment on  events"""
     data = request.get_json()
@@ -218,26 +222,26 @@ def commentOnEvent(eventid):
     if not eventrecord:
         return jsonify({'message':'This event does not exist!'})
     else:
-        comment= Comment(comment_publicId=str(uuid.uuid4()),eventid=eventid,guestid=data['guestid'],comment=data['comment'])
+        comment= Comment(public_id=str(uuid.uuid4()),eventid=int(eventid),guestid=data['guestid'],comment=data['comment'])
         db.session.add(comment)
         db.session.commit()
         return jsonify({'message': 'comment was added to Event'})
 
-@app.route("/api/events/<eventid>/rate",methods="POST")
+@app.route("/api/events/<eventid>/rate",methods=["POST","PUT"])
 def rateEvent(eventid):
     """ rating an events"""
+    data = request.get_json()
     eventrecord =  Event.query.filter_by(eventid=eventid).first()
     if not eventrecord:
         return jsonify({'message':'This event does not exist!'})
     else:
         raterecord = Rating.query.filter_by(eventid=eventid).first()
-        rate=""
         if raterecord != None:
             oldrate=raterecord.rate_value
-            rate= Rating(rating_publicId=data["rating_publicId"], eventid=eventid, rate_value= (old+data['rate_value'])/2)
+            raterecord.rate_value=(oldrate+data['rate_value'])/2
         else:
-            rate= Rating(rating_publicId=data["rating_publicId"], eventid=eventid, rate_value= data['rate_value'])
-        db.session.add(rate)
+            rate= Rating(eventid=eventid, rate_value= data['rate_value'])
+            db.session.add(rate)
         db.session.commit()
         return jsonify({'message': 'rating was added to Event'})
 
@@ -245,17 +249,7 @@ def rateEvent(eventid):
 
 
 
-# user_loader callback. This callback is used to reload the user object from
-# the user ID stored in the session
-@login_manager.user_loader
-def load_user(id):
-    return UserProfile.query.get(int(id))
 
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-# Here we define a function to collect form errors from Flask-WTF
 # which we can later use
 def flash_errors(form):
     for field, errors in form.errors.items():
@@ -286,7 +280,3 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
